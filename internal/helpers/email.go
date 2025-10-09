3d210e5a4e7a12ef0c896c01e148ed74a7afeac5
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-10-09 20:59:16 -0300

    feature: added impl for sending emails using sendgrid api

--- internal/helpers/email.go
+++ internal/helpers/email.go
@@ -1,8 +1,26 @@
 package helpers
 
-import "net/mail"
+import (
+	n "net/mail"
+	"os"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
 
 func ValidateEmail(email string) bool {
-	_, err := mail.ParseAddress(email)
+	_, err := n.ParseAddress(email)
 	return err == nil
+}
+
+func SendMail(toEmail, toName, subject, body string) error {
+	from := mail.NewEmail("Financial CLI", "me@pedrooyarzun.xyz")
+	to := mail.NewEmail(toName, toEmail)
+
+	message := mail.NewSingleEmail(from, subject, to, body, body)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+
+	_, err := client.Send(message)
+
+	return err
 }
