0814618328a2d11214f3a555886bacdcf5eb9c2b
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-10-09 22:52:52 -0300

    feature: added user service with signUp method

--- internal/services/user.go
+++ internal/services/user.go
@@ -1,1 +1,71 @@
 package services
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/api/dto"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/domain"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/helpers"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/repositories"
+)
+
+type UserService interface {
+	SignUp(usr dto.SignUpReq) error
+	SignIn(usr dto.SignInReq) error
+}
+
+type userService struct {
+	ur repositories.UserRepository
+}
+
+func NewUserService(ur repositories.UserRepository) UserService {
+	return &userService{
+		ur,
+	}
+}
+
+func (s *userService) SignUp(req dto.SignUpReq) error {
+
+	//Check if user is on DB
+	res := s.ur.GetByEmail(req.Email)
+
+	if res.Name != "" {
+		return ErrUserAlreadyExists
+	}
+
+	password, _ := helpers.HashPassword(req.Password)
+
+	//Auth token
+	token := uuid.New().String()
+
+	usr := domain.User{
+		Name:                       req.Name,
+		Email:                      req.Email,
+		Password:                   password,
+		VerificationToken:          token,
+		VerificationTokenExpiresAt: time.Now().Add(5 * time.Hour),
+		Verified:                   false,
+		Deleted:                    false,
+	}
+
+	//Save user on db
+	err := s.ur.Create(usr)
+
+	if err != nil {
+		return ErrUserCreationFailed
+	}
+
+	//Send email to user
+	err = helpers.SendMail(req.Email, req.Name, "Example", "Example")
+
+	if err != nil {
+		return ErrUserCreationFailed
+	}
+
+	return nil
+}
+
+func (s *userService) SignIn(dto.SignInReq) error {
+	return nil
+}
