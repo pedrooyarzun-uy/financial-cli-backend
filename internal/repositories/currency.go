2d70f383ca7a0019d03fe654070961811338c2d7
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-10-27 00:16:54 -0300

    feature: added currency repository

---
+++ internal/repositories/currency.go
@@ -0,0 +1,26 @@
+package repositories
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/domain"
+)
+
+type CurrencyRepository interface {
+	GetAll() []domain.Currency
+}
+
+type currencyRepository struct {
+	db *sqlx.DB
+}
+
+func NewCurrencyRepository(db *sqlx.DB) CurrencyRepository {
+	return &currencyRepository{db: db}
+}
+
+func (r *currencyRepository) GetAll() []domain.Currency {
+	currencies := []domain.Currency{}
+
+	r.db.Select(currencies, "SELECT * FROM currency ORDER BY id ASC;")
+
+	return currencies
+}
