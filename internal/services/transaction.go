e2ef8e6afe59845d39b90c33c5a0df3b37380e11
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-10-26 22:58:52 -0300

    feature: added transaction service! add method implemented

---
+++ internal/services/transaction.go
@@ -0,0 +1,45 @@
+package services
+
+import (
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/api/dto"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/domain"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/repositories"
+)
+
+type TransactionService interface {
+	Add(req dto.AddTransactionReq) error
+}
+
+type transactionService struct {
+	tr repositories.TransactionRepository
+	ar repositories.AccountRepository
+}
+
+func NewTransactionRepository(tr repositories.TransactionRepository, ar repositories.AccountRepository) TransactionService {
+	return &transactionService{
+		tr, ar,
+	}
+}
+
+func (s *transactionService) Add(req dto.AddTransactionReq) error {
+	transaction := domain.Transaction{
+		Notes:       req.Notes,
+		Amount:      req.Amount,
+		Account:     req.Account,
+		Currency:    req.Currency,
+		Category:    req.Category,
+		Subcategory: req.Subcategory,
+		Type:        req.Type,
+	}
+
+	accountCurrency := s.ar.GetCurrency(req.Account)
+
+	if req.Currency != accountCurrency || accountCurrency == 0 {
+		return ErrTransactionNotCorrectCurrency
+	}
+
+	err := s.tr.Add(transaction)
+
+	return err
+
+}
