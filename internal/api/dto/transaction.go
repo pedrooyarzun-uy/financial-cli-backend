e2a049178e396859f4746f48c3dd3ef2f6ddb34b
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-10-26 22:54:57 -0300

    fix: added fix for not requiring all fields

--- internal/api/dto/transaction.go
+++ internal/api/dto/transaction.go
@@ -1,11 +1,11 @@
 package dto
 
-type AddReq struct {
-	Notes       string  `json:"notes" binding:"required"`
+type AddTransactionReq struct {
+	Notes       string  `json:"notes"`
 	Amount      float64 `json:"amount" binding:"required"`
 	Account     int     `json:"account" binding:"required"`
-	Currency    int     `json:"currenct" binding:"required"`
-	Category    int     `json:"category" binding:"required"`
-	Subcategory int     `json:"subcategory" binding:"required"`
+	Currency    int     `json:"currency" binding:"required"`
+	Category    int     `json:"category"`
+	Subcategory int     `json:"subcategory"`
 	Type        int     `json:"type" binding:"required"`
 }
