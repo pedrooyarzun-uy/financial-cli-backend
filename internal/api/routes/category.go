570d262a890c30ba29d5b4bf21ed155d747088d9
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-11-02 13:38:09 -0300

    feature: added new category route

---
+++ internal/api/routes/category.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/api/dto"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/api/middlewares"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/services"
+)
+
+func NewCategoryRoutes(mux *http.ServeMux, s services.CategoryService) {
+	getAllCategories(mux, s)
+}
+
+func getAllCategories(mux *http.ServeMux, s services.CategoryService) {
+	mux.Handle("/category/get-all", middlewares.AuthMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			res := s.GetAll()
+
+			response := dto.GetAllCategoryRes{}
+
+			w.WriteHeader(200)
+			response.Message = "ok"
+			response.Categories = res
+			json.NewEncoder(w).Encode(response)
+		}
+	})))
+}
