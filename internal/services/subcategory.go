e0f21630395a3268985f7c87d4168043f1d597ce
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-07 19:40:47 -0300

    feature: added subcategory service

---
+++ internal/services/subcategory.go
@@ -0,0 +1,26 @@
+package services
+
+import (
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/domain"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/repositories"
+)
+
+type SubcategoryService interface {
+	GetSubcategoriesByCategory(categoryId int) ([]domain.Subcategory, error)
+}
+
+type subcategoryService struct {
+	sr repositories.SubcategoryRepository
+}
+
+func NewSubcategoryService(sr repositories.SubcategoryRepository) SubcategoryService {
+	return &subcategoryService{
+		sr,
+	}
+}
+
+func (s *subcategoryService) GetSubcategoriesByCategory(categoryId int) ([]domain.Subcategory, error) {
+
+	return s.sr.GetSubcategoriesByCategory(categoryId)
+
+}
