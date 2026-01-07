f503689bcca080e55c6e52957864abc80b345dc9
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-07 19:43:25 -0300

    feature: added routes for subcategories

---
+++ internal/api/routes/subcategory.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/api/dto"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/api/middlewares"
+	"github.com/pedrooyarzun-uy/financial-cli-backend/internal/services"
+)
+
+func NewSubcategoryRoutes(mux *http.ServeMux, s services.SubcategoryService) {
+
+}
+
+func getSubcategoriesByCategory(mux *http.ServeMux, s services.SubcategoryService) {
+	mux.Handle("/subcategory/get-by-category", middlewares.AuthMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+
+			rawId := r.URL.Query().Get("id")
+			parsedId, _ := strconv.Atoi(rawId)
+
+			categories, err := s.GetSubcategoriesByCategory(parsedId)
+
+			response := dto.GetSubcategoriesByCategoryRes{}
+
+			if err != nil {
+				w.WriteHeader(400)
+				response.Message = err.Error()
+			} else {
+				w.WriteHeader(200)
+				response.Message = "ok"
+			}
+
+			response.Subcategories = categories
+			json.NewEncoder(w).Encode(response)
+
+		}
+	})))
+}
