914fd269d499517c4e750c21d6c76bc4117a7cca
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-07 19:17:26 -0300

    feature: added domain for subcategories

---
+++ internal/domain/subcategory.go
@@ -0,0 +1,8 @@
+package domain
+
+type Subcategory struct {
+	Id         int    `db:"id"`
+	Name       string `db:"name"`
+	CategoryId int    `db:"category_id"`
+	UserId     int    `db:"user_id"`
+}
