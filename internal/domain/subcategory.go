dfe7afcafa3ecf211e6005567faff1a18fc87976
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-07 19:52:12 -0300

    fix: added fix for allowing null results on subcategory

--- internal/domain/subcategory.go
+++ internal/domain/subcategory.go
@@ -4,5 +4,5 @@ type Subcategory struct {
 	Id         int    `db:"id"`
 	Name       string `db:"name"`
 	CategoryId int    `db:"category_id"`
-	UserId     int    `db:"user_id"`
+	UserId     *int   `db:"user_id"`
 }
