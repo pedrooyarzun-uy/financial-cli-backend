c22bee6b706be5a74aaf868c104ea2b096a004fe
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-04-05 23:00:03 -0300

    feature: added domain of credit_card impl

---
+++ internal/domain/credit_card.go
@@ -0,0 +1,15 @@
+package domain
+
+import "time"
+
+type CreditCard struct {
+	Id         int       `db:"id"`
+	Name       string    `db:"name"`
+	BankID     int       `db:"bankID"`
+	CurrencyID int       `db:"currencyID"`
+	OwnerID    int       `db:"ownerID"`
+	CloseDay   time.Time `db:"close_day"`
+	DueDay     time.Time `db:"due_day"`
+	Limit      float64   `db:"limit"`
+	CreatedAt  time.Time `db:"created_at"`
+}
