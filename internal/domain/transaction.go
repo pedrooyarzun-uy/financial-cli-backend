b19f3b7389e8ae96807f669d7bfc4c029c42212c
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-04-26 01:41:20 -0300

    feature: added new domain of transaction

--- internal/domain/transaction.go
+++ internal/domain/transaction.go
@@ -3,13 +3,30 @@ package domain
 import "time"
 
 type Transaction struct {
-	Id          int       `db:"id"`
-	Notes       string    `db:"notes"`
-	Amount      float64   `db:"amount"`
-	Account     int       `db:"account"`
-	Currency    int       `db:"currency"`
-	Category    int       `db:"category"`
-	Subcategory int       `db:"subcategory"`
-	Type        int       `db:"type"`
-	CreatedAt   time.Time `db:"created_at"`
+	Id            int             `db:"id"`
+	Notes         string          `db:"notes"`
+	Amount        float64         `db:"amount"`
+	Kind          TransactionKind `db:"kind"`
+	PaymentMethod PaymentMethod   `db:"payment_method"`
+	CurrencyId    int             `db:"currency_id"`
+	CategoryId    int             `db:"category_id"`
+	SubcategoryId int             `db:"subcategory_id"`
+	AccountId     int             `db:"account_id"`
+	CreditCardId  int             `db:"credit_card_id"`
+	CreatedAt     time.Time       `db:"created_at"`
 }
+
+type PaymentMethod string
+
+const (
+	PaymentMethodAccount    PaymentMethod = "account"
+	PaymentMethodCreditCard PaymentMethod = "credit_card"
+	PaymentMethodCash       PaymentMethod = "cash"
+)
+
+type TransactionKind string
+
+const (
+	TransactionKindIncome  TransactionKind = "income"
+	TransactionKindExpense TransactionKind = "expense"
+)
